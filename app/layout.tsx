7220de438808f1eda7a6ce0c519de89932745a1a
Preet Panchal <preet2020@icloud.com>
2023-02-24 00:13:51 -0500

    main

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 "use client"
-import { ChakraProvider } from '@chakra-ui/react'
-
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { ChakraProvider } from "@chakra-ui/react"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 export default function RootLayout({
   children,
